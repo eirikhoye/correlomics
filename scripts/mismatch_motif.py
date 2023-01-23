e8188badb2e4eb161653d972963e789ec35a433d
eirikhoye <eirikhoye@gmail.com>
2023-01-23 10:09:05 +0000

    added_mismatch_motif.py

---
+++ scripts/mismatch_motif.py
@@ -0,0 +1,6 @@
+from subprocess import *
+from Bio import SeqIO
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from matplotlib.colors import LinearSegmentedColormap
