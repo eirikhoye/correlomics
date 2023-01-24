2d6a16135a271e66fd3289bfbf7a737fd68067a3
eirikhoye <eirikhoye@gmail.com>
2023-01-24 08:35:32 +0000

    start24jan23

---
+++ correlomics/foldings.py
@@ -0,0 +1,15 @@
+from subprocess import *
+from Bio import SeqIO
+
+class foldings():
+    def __init__(self, mirgenedb_id):
+        self.mirgenedb_id = mirgenedb_id
+        #self.pri_mirna = # make function to fetch primirna
+
+    #def get_sequences():
+        
+    
+    #def fold():
+
+    #    workingfile = open("tmp/workingfile.txt", "w")
+    #    workingfile.write(str(self.pri_mirna))
