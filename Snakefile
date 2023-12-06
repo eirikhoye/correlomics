5b573222f4ff616af78f00a9e81a48c834918206
eirikhoye <eirikhoye@gmail.com>
2023-12-06 19:10:57 +0000

    added Snakefile and reference_extended_matu

--- Snakefile
+++ Snakefile
@@ -1,7 +1,16 @@
+#configfile: "config.yaml"
+
 rule extend_mature_star_pre_ref:
     """
     Extend all MirGeneDB mature and star sequences with 5nt 5p and 3p, 
     using precursors as reference
     """
     input:
-        
+        "/home/jcdenton/projects/mirgenedb_database/merged_all-pri-30-30.fas",
+        "/home/jcdenton/projects/mirgenedb_database/mirgenedb_mature_star_genome/{species_id}.fas"
+    output:
+        "/home/jcdenton/projects/mirgenedb_database/mirgenedb_merged_extended_genome/{species_id}.fas"
+    conda:
+        "envs/biopython.yaml"
+    shell:
+        "python scripts/reference_extended_mature_star_maker.py {input} {output}"
--- scripts/reference_extended_mature_star_maker.py
+++ scripts/reference_extended_mature_star_maker.py
@@ -1,14 +1,17 @@
 from Bio.SeqIO.FastaIO import SimpleFastaParser
 from Bio import SeqIO
 import os
+import sys
 
 """
 A python script to extend all MirGeneDB mature and star sequences
 with 5nt 5prime and 3prime, from the precursor sequences.
 """
 
-pri_mirna = '/home/jcdenton/projects/mirgenedb_database/merged_all-pri-30-30.fas'
-pri_mirna_dict = SeqIO.to_dict(SeqIO.parse(pri_mirna, 'fasta'))
+pri_mirna_dict = SeqIO.to_dict(SeqIO.parse(sys.argv[1], 'fasta'))
+mature_star = sys.argv[2]
+#logfile = open("/home/jcdenton/projects/mirgenedb_database/mirgenedb_merged_extended_genome/logfile.txt", 'w+') ### consider adding it back
+outfile = open(sys.argv[3], 'w')
 
 def naive(p, t):
 	start = 0
@@ -25,41 +28,34 @@ def naive(p, t):
 	end = start + len(p)
 	return({'start' : start, 'end' : end})
 
-logfile = open('/home/jcdenton/projects/mirgenedb_database/mirgenedb_merged_extended_genome/logfile.txt', 'w+')
-
-for mature_star in os.listdir('/home/jcdenton/projects/mirgenedb_database/mirgenedb_mature_star_genome/'):
-    if not mature_star.endswith('.fas'):
-        continue
-    print(mature_star)
-    outfile = open('/home/jcdenton/projects/mirgenedb_database/mirgenedb_merged_extended_genome/' + str(mature_star), 'w+')
-    with open('/home/jcdenton/projects/mirgenedb_database/mirgenedb_mature_star_genome/' + mature_star, 'r') as file:
-        for ID, SEQ in SimpleFastaParser(file):
-            strand = ''
-            if ID.endswith('*'):
-                strand = 'star'
-            if ID.endswith('p'):
-                strand = 'mature'
-            if ID.endswith('pre'):
-                strand = 'pre'
-            if strand == 'pre':
-                continue
-            if strand == 'mature' and ID[:-3] + '_pri' not in pri_mirna_dict:
-                print(mature_star, ID, 'nope')
-                logfile.write(mature_star + ID + ' no_pri_sequence_found\n')
-                continue
-            if strand == 'star' and ID[:-4] + '_pri' not in pri_mirna_dict:
-                print(mature_star, ID, 'nope')
-                logfile.write(mature_star + ID + ' no_pri_sequence_found\n')
-                continue
-            outfile.write('>' + ID + '\n')
-            if strand == 'mature':
-                start_pos = naive(str(SEQ), str(pri_mirna_dict[ID[:-3] + '_pri'].seq))['start'] - 5
-                end_pos   = naive(str(SEQ), str(pri_mirna_dict[ID[:-3] + '_pri'].seq))['end'] + 5
-                outfile.write(str(pri_mirna_dict[ID[:-3] + '_pri'].seq[start_pos : end_pos]) + '\n')
-            if strand == 'star':
-                start_pos = naive(str(SEQ), str(pri_mirna_dict[ID[:-4] + '_pri'].seq))['start'] - 5
-                end_pos   = naive(str(SEQ), str(pri_mirna_dict[ID[:-4] + '_pri'].seq))['end'] + 5
-                outfile.write(str(pri_mirna_dict[ID[:-4] + '_pri'].seq[start_pos : end_pos]) + '\n')
+with open(mature_star, 'r') as file:
+    for ID, SEQ in SimpleFastaParser(file):
+        strand = ''
+        if ID.endswith('*'):
+            strand = 'star'
+        if ID.endswith('p'):
+            strand = 'mature'
+        if ID.endswith('pre'):
+            strand = 'pre'
+        if strand == 'pre':
+            continue
+        if strand == 'mature' and ID[:-3] + '_pri' not in pri_mirna_dict:
+            print(mature_star, ID, 'nope')
+            #logfile.write(mature_star + ID + ' no_pri_sequence_found\n')
+            continue
+        if strand == 'star' and ID[:-4] + '_pri' not in pri_mirna_dict:
+            print(mature_star, ID, 'nope')
+            #logfile.write(mature_star + ID + ' no_pri_sequence_found\n')
+            continue
+        outfile.write('>' + ID + '\n')
+        if strand == 'mature':
+            start_pos = naive(str(SEQ), str(pri_mirna_dict[ID[:-3] + '_pri'].seq))['start'] - 5
+            end_pos   = naive(str(SEQ), str(pri_mirna_dict[ID[:-3] + '_pri'].seq))['end'] + 5
+            outfile.write(str(pri_mirna_dict[ID[:-3] + '_pri'].seq[start_pos : end_pos]) + '\n')
+        if strand == 'star':
+            start_pos = naive(str(SEQ), str(pri_mirna_dict[ID[:-4] + '_pri'].seq))['start'] - 5
+            end_pos   = naive(str(SEQ), str(pri_mirna_dict[ID[:-4] + '_pri'].seq))['end'] + 5
+            outfile.write(str(pri_mirna_dict[ID[:-4] + '_pri'].seq[start_pos : end_pos]) + '\n')
 
 
 #for star in os.listdir('mirgenedb_star_genome/'):
