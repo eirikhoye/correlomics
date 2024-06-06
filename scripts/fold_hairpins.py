f86e8c1676943c9bf3f730d8e992c30ebbf78428
eirikhoye <eirikhoye@gmail.com>
2024-06-06 07:43:35 +0000

    updated fold hairpins to yaml config

--- scripts/fold_hairpins.py
+++ scripts/fold_hairpins.py
@@ -1,67 +1,97 @@
-import pickle
 import os
 import sys
+import yaml
+import pickle
+import argparse
+import pandas as pd
 from Bio import SeqIO
 
 current_dir = os.path.dirname(__file__)
 parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
 sys.path.append(parent_dir)
-
-# Now you can import your module
 from correlomics.foldings import Fold
 
 
-"""
-Define input files
-"""
-stemfile   = open('/home/jcdenton/projects/mirgenedb_database/merged_all-pri-30-30.fas', 'r')
-maturefile = open('/home/jcdenton/projects/mirgenedb_database/mature.fas', 'r') 
-starfile   = open('/home/jcdenton/projects/mirgenedb_database/star.fas')
-loopfile   = open('/home/jcdenton/projects/mirgenedb_database/loopfile.fas')
-stem   = SeqIO.to_dict(SeqIO.parse(stemfile, "fasta"))
-mature = SeqIO.to_dict(SeqIO.parse(maturefile, "fasta"))
-star   = SeqIO.to_dict(SeqIO.parse(starfile, "fasta"))
-loop   = SeqIO.to_dict(SeqIO.parse(loopfile, "fasta"))
+def load_config(config_file):
+    """Load configuration from a YAML file."""
+    with open(config_file, 'r') as file:
+        config = yaml.safe_load(file)
+        return config
 
 
-outdir = {
-    'no_constraints': '/home/jcdenton/projects/correlomics/test_let7_no_constraints/',
-    'restrict_lower_stem': '/home/jcdenton/projects/correlomics/test_let7_restrict_lower_stem/',
-    'restrict_lower_stem_loop': '/home/jcdenton/projects/correlomics/test_let7_restrict_lower_stem_loop/',
-    'restrict_loop': '/home/jcdenton/projects/correlomics/test_let7_restrict_loop/'
-}
+def load_sequences(file_paths):
+    """Load sequences from given file paths."""
+    sequences = {}
+    for key, file_path in file_paths.items():
+        with open(file_path, 'r') as file:
+            sequences[key] = SeqIO.to_dict(SeqIO.parse(file, "fasta"))
+    return sequences
 
 
-outdir = {
-    'no_constraints': '/home/jcdenton/projects/correlomics/test_no_constraints/',
-    'restrict_lower_stem': '/home/jcdenton/projects/correlomics/test_restrict_lower_stem/',
-    'restrict_lower_stem_loop': '/home/jcdenton/projects/correlomics/test_restrict_lower_stem_loop/',
-    'restrict_loop': '/home/jcdenton/projects/correlomics/test_restrict_loop/'
-}
-
-
-# Process hairpins
-for id in stem.keys():
-    if "Mir-451" in id:
-        continue
-
-    hairpin = Fold(id[:-4])
-    hairpin.fold_hairpin()
-    with open(outdir['no_constraints'] + hairpin.mirgenedb_id + '.pkl', 'wb') as f:
+def save_hairpin(hairpin, output_dir, suffix):
+    os.makedirs(output_dir, exist_ok=True)  # Ensure the output directory exists
+    file_path = os.path.join(output_dir, f"{hairpin.mirgenedb_id}.pkl")
+    with open(file_path, 'wb') as f:
         pickle.dump([hairpin.mirgenedb_id+'\n', hairpin.seq, hairpin.dot, hairpin.dG], f)
 
-    hairpin = Fold(id[:-4])
-    hairpin.fold_hairpin(constrain_stem=True)
-    with open(outdir['restrict_lower_stem'] + hairpin.mirgenedb_id + '.pkl', 'wb') as f:
-        pickle.dump([hairpin.mirgenedb_id+'\n', hairpin.seq, hairpin.dot, hairpin.dG], f)
 
-    hairpin = Fold(id[:-4])
-    hairpin.fold_hairpin(constrain_stem=True, constrain_loop=True)
-    with open(outdir['restrict_lower_stem_loop'] + hairpin.mirgenedb_id + '.pkl', 'wb') as f:
-        pickle.dump([hairpin.mirgenedb_id+'\n', hairpin.seq, hairpin.dot, hairpin.dG], f)
+def process_hairpins(sequences, output_dirs, filters):
+    """Process hairpins and save them to respective directories."""
+    misfold_dict = {}
+    exclude_list = filters.get('exclude', [])
+    include_only_list = filters.get('include_only', [])
 
-    hairpin = Fold(id[:-4])
-    hairpin.fold_hairpin(constrain_stem=False, constrain_loop=True)
-    with open(outdir['restrict_loop'] + hairpin.mirgenedb_id + '.pkl', 'wb') as f:
-        pickle.dump([hairpin.mirgenedb_id+'\n', hairpin.seq, hairpin.dot, hairpin.dG], f)
+    for id in sequences['pri_mirna'].keys():
+        if any(excl in id for excl in exclude_list):
+            continue
+        if include_only_list and not any(inc in id for inc in include_only_list):
+            continue
 
+        misfold_dict[id] = [0, 0, 0, 0]
+        constraints = [
+            (False, False, 'none'),
+            (True, False, 'stem'),
+            (True, True, 'both'),
+            (False, True, 'loop')
+        ]
+
+        for i, (constrain_stem, constrain_loop, suffix) in enumerate(constraints):
+            hairpin = Fold(id[:-4])
+            hairpin.fold_hairpin(constrain_stem=constrain_stem, constrain_loop=constrain_loop)
+            save_hairpin(hairpin, output_dirs[suffix], suffix)
+            if hairpin.check_misfold():
+                misfold_dict[id][i] = 1
+
+    return misfold_dict
+
+
+def main():
+    # Define command line arguments
+    parser = argparse.ArgumentParser(description='Fold pri-miRNA hairpin structures')
+    parser.add_argument('config_file', type=str, help='Path to YAML config')
+    args = parser.parse_args()
+
+    # Load configuration from YAML file
+    config = load_config(args.config_file)
+    base_path = config['file_paths']['base_path']
+    file_paths = {
+        key: os.path.join(base_path, relative_path)
+        for key, relative_path in config['file_paths']['fasta_files'].items()
+    }
+
+    sequences = load_sequences(file_paths)
+
+    output_dirs = {
+        key: relative_path
+        for key, relative_path in config['out_dir'].items()
+    }
+
+    filters = config.get('filters', {})
+    misfold_dict = process_hairpins(sequences, output_dirs, filters)
+
+    df = pd.DataFrame.from_dict(misfold_dict, orient='index', columns=['none', 'stem', 'loop', 'both'])
+    print(df)
+
+
+if __name__ == "__main__":
+    main()
--- scripts/old/fold_hairpins.py
+++ scripts/old/fold_hairpins.py
@@ -14,7 +14,7 @@ from correlomics.foldings import Fold
 """
 Define input files
 """
-stemfile   = open('/home/jcdenton/projects/mirgenedb_database/precursor.fas', 'r')
+stemfile   = open('/home/jcdenton/projects/mirgenedb_database/merged_all-pri-30-30.fas', 'r')
 maturefile = open('/home/jcdenton/projects/mirgenedb_database/mature.fas', 'r') 
 starfile   = open('/home/jcdenton/projects/mirgenedb_database/star.fas')
 loopfile   = open('/home/jcdenton/projects/mirgenedb_database/loopfile.fas')
@@ -22,30 +22,14 @@ stem   = SeqIO.to_dict(SeqIO.parse(stemfile, "fasta"))
 mature = SeqIO.to_dict(SeqIO.parse(maturefile, "fasta"))
 star   = SeqIO.to_dict(SeqIO.parse(starfile, "fasta"))
 loop   = SeqIO.to_dict(SeqIO.parse(loopfile, "fasta"))
-outdir = '/home/jcdenton/projects/mirgenedb_database/hairpins_let7_pre/'
 
 
-### debug
-outdir = '/home/jcdenton/projects/correlomics/test_let7_pre/'
-
-###
-
-# Open output files
-output_files = {
-    'no_constraints': open('misfold_no_constraints.txt', 'w'),
-    'constrain_lower_stem': open('misfold_constrain_lower_stem.txt', 'w'),
-    'constrain_lower_stem_and_loop': open('misfold_constrain_lower_stem_and_loop.txt', 'w')
+outdir = {
+    'no_constraints': '/home/jcdenton/projects/mirgenedb_database/hairpins/test_no_constraints/',
+    'restrict_lower_stem': '/home/jcdenton/projects/mirgenedb_database/hairpins/test_restrict_lower_stem/',
+    'restrict_lower_stem_loop': '/home/jcdenton/projects/mirgenedb_database/hairpins/test_restrict_lower_stem_loop/',
+    'restrict_loop': '/home/jcdenton/projects/mirgenedb_database/hairpins/test_restrict_loop/'
 }
-
-
-def process_hairpin(hairpin, output_file):
-    if hairpin.check_misfold() is False:
-        with open(outdir + hairpin.mirgenedb_id + '.pkl', 'wb') as f:
-            pickle.dump([hairpin.seq, hairpin.dot, hairpin.dG], f)
-    else:
-        output_file.write(hairpin.id + '\n')
-        output_file.write(hairpin.seq)
-        output_file.write(hairpin.dot)
 
 
 # Process hairpins
@@ -53,27 +37,23 @@ for id in stem.keys():
     if "Mir-451" in id:
         continue
 
-    if "Hsa-Let-7" not in id:
-        continue
+    hairpin = Fold(id[:-4])
+    hairpin.fold_hairpin()
+    with open(outdir['no_constraints'] + hairpin.mirgenedb_id + '.pkl', 'wb') as f:
+        pickle.dump([hairpin.mirgenedb_id+'\n', hairpin.seq, hairpin.dot, hairpin.dG], f)
 
+    hairpin = Fold(id[:-4])
+    hairpin.fold_hairpin(constrain_stem=True)
+    with open(outdir['restrict_lower_stem'] + hairpin.mirgenedb_id + '.pkl', 'wb') as f:
+        pickle.dump([hairpin.mirgenedb_id+'\n', hairpin.seq, hairpin.dot, hairpin.dG], f)
 
+    hairpin = Fold(id[:-4])
+    hairpin.fold_hairpin(constrain_stem=True, constrain_loop=True)
+    with open(outdir['restrict_lower_stem_loop'] + hairpin.mirgenedb_id + '.pkl', 'wb') as f:
+        pickle.dump([hairpin.mirgenedb_id+'\n', hairpin.seq, hairpin.dot, hairpin.dG], f)
 
-    hairpin = Fold(id[:-4])  # drop miRNA without loop seq
-    if hairpin.mirgenedb_id + '_loop' not in loop.keys():
-        continue
+    hairpin = Fold(id[:-4])
+    hairpin.fold_hairpin(constrain_stem=False, constrain_loop=True)
+    with open(outdir['restrict_loop'] + hairpin.mirgenedb_id + '.pkl', 'wb') as f:
+        pickle.dump([hairpin.mirgenedb_id+'\n', hairpin.seq, hairpin.dot, hairpin.dG], f)
 
-    hairpin.fold_hairpin()  # no constraints
-    if not hairpin.check_misfold():
-        process_hairpin(hairpin, output_files['no_constraints'])
-    else:
-        hairpin.fold_hairpin(constrain_stem=True)  # Constrain lower stem to not fold
-        if not hairpin.check_misfold():
-            process_hairpin(hairpin, output_files['constrain_lower_stem'])
-        else:
-            hairpin.fold_hairpin(constrain_stem=True, constrain_loop=True)  # Constrain lower stem and loop to not fold
-            if not hairpin.check_misfold():
-                process_hairpin(hairpin, output_files['constrain_lower_stem_and_loop'])
-
-# Close output files
-for file in output_files.values():
-    file.close()
